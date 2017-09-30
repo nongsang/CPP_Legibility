6327f022c53b0f48081519927c0d81c3e65749d4
nongsang <shdtkd37@gmail.com>
2017-09-30 17:09:44 +0900

    9월 30일 변수 이름

---
+++ 01.variable_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float t;			// 변수 이름으로 어떤 용도의 변수인지 알 수 없다.
+	float time;			// 시간과 관련된 변수라는 것을 변수 이름만으로 알 수 있다.
+	float limitTime;	// 좀 더 구체적으로 제한시간 전용 변수라는 것을 알 수 있다.
+
+	int matrix[4][4];
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			matrix[i][j] = 0;		// 배열의 인덱스 전용일 경우 i, j, k등 짧은 이름으로 쓰자.
+}
